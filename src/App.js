5fefd77cb63b1463bc52de1d9303355be3f7a951
chagansingh <chagansingh9876@gmail.com>
2023-02-06 20:36:55 +0530

    hooks added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
+
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './Store/auth-context';
+import AuthContext from './store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider
-    value={{
-      isLoggedIn: isLoggedIn,
-      onLogout:logoutHandler
-    }}
-  >
-    <MainHeader/>
-    <main>
-      {!isLoggedIn && <Login onLogin={loginHandler} />}
-      {isLoggedIn && <Home onLogout={logoutHandler} />}
-    </main>
-  </AuthContext.Provider>
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
   );
 }
 
